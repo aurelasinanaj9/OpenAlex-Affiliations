a6becaea638931cb0813b7869a7d561a666c80b7
aurelasinanaj <aurela.sinanaj@studio.unibo.it>
2023-07-11 15:36:40 +0200

    cleaning

--- stage_2.py
+++ stage_2.py
@@ -16,7 +16,7 @@ with gzip.open('output/output.json.gz', 'rt') as f,\
         gzip.open('sample.csv.gz', 'wt') as sample:
             
             writer = csv.writer(sample)
-            writer.writerow(['oa_id', 'oa_count', 'affiliation_string', 'ror_id'])  # Header
+            writer.writerow(['oa_id', 'oa_count', 'affiliation_string', 'ror_id'])  # header
 
             for line in f:
                 unique_combinations = set()
@@ -26,7 +26,7 @@ with gzip.open('output/output.json.gz', 'rt') as f,\
 
                 for authorship in data['authorships']:
 
-                    ror_list = []  # List to store multiple ror values
+                    ror_list = []  # list to store ror values
 
                     for institution in authorship['institutions']:
                         ror = institution.get('ror')
@@ -50,58 +50,4 @@ with gzip.open('output/output.json.gz', 'rt') as f,\
                         
                         
                         
-                        
-                        
-                        
-                        
-# data1 = {"id": "https://openalex.org/W2740742622", "doi": "https://doi.org/10.1016/j.cor.2018.04.013", "title": "A shortest-path-based approach for the stochastic knapsack problem with non-decreasing expected overfilling costs", "authorships": [{"author_position": "first", "author": {"id": "https://openalex.org/A2136675481", "display_name": "Troels Martin Range", "orcid": "https://orcid.org/0000-0002-5183-6778"}, "institutions": [{"id": "https://openalex.org/I204778367", "display_name": "Norwegian University of Science and Technology", "ror": "https://ror.org/05xg72x27", "country_code": "NO", "type": "education"}, {"id": "https://openalex.org/I4210134581", "display_name": "Hospital South West Jutland", "ror": "https://ror.org/03pzgk858", "country_code": "DK", "type": "healthcare"}], "is_corresponding": None, "raw_affiliation_string": "Department of Industrial Economics and Technology Management, Norwegian University of Science and Technology, Alfred Getz veg 3, Trondheim, NO-7491, Norway; Hospital of South West Jutland and Institute of Regional Health Research, Centre of South West Jutland, University of Southern Denmark, Finsensgade 35, Esbjerg DK-6700, Denmark"}, {"author_position": "middle", "author": {"id": "https://openalex.org/A2668814171", "display_name": "Dawid Kozlowski", "orcid": None}, "institutions": [{"id": None, "display_name": "Institut for Virksomhedsledelse og \u00d8konomi", "ror": None, "country_code": None, "type": None}], "is_corresponding": None, "raw_affiliation_string": "Institut for Virksomhedsledelse og \u00d8konomi"}, {"author_position": "last", "author": {"id": "https://openalex.org/A2156506640", "display_name": "Niels Petersen", "orcid": "https://orcid.org/0000-0001-6272-6059"}, "institutions": [{"id": "https://openalex.org/I177969490", "display_name": "University of Southern Denmark", "ror": "https://ror.org/03yrrjy16", "country_code": "DK", "type": "education"}], "is_corresponding": None, "raw_affiliation_string": "Department of Business and Economics, and COHERE, University of Southern Denmark, Campusvej 55, Odense M, DK-5230, Denmark"}], "publication_year": 2018}
-
-# data2 = {"id": "https://openalex.org/W2740742623", "doi": "https://doi.org/10.1016/j.cor.2018.04.013", "title": "A shortest-path-based approach for the stochastic knapsack problem with non-decreasing expected overfilling costs", "authorships": [{"author_position": "first", "author": {"id": "https://openalex.org/A2136675481", "display_name": "Troels Martin Range", "orcid": "https://orcid.org/0000-0002-5183-6778"}, "institutions": [{"id": "https://openalex.org/I204778367", "display_name": "Norwegian University of Science and Technology", "ror": "https://ror.org/05xg72x27", "country_code": "NO", "type": "education"}, {"id": "https://openalex.org/I4210134581", "display_name": "Hospital South West Jutland", "ror": "https://ror.org/03pzgk858", "country_code": "DK", "type": "healthcare"}], "is_corresponding": None, "raw_affiliation_string": "Department of Industrial Economics and Technology Management, Norwegian University of Science and Technology, Alfred Getz veg 3, Trondheim, NO-7491, Norway; Hospital of South West Jutland and Institute of Regional Health Research, Centre of South West Jutland, University of Southern Denmark, Finsensgade 35, Esbjerg DK-6700, Denmark"}, {"author_position": "middle", "author": {"id": "https://openalex.org/A2668814171", "display_name": "Dawid Kozlowski", "orcid": None}, "institutions": [{"id": None, "display_name": "Institut for Virksomhedsledelse og \u00d8konomi", "ror": None, "country_code": None, "type": None}], "is_corresponding": None, "raw_affiliation_string": "Institut for Virksomhedsledelse og \u00d8konomi"}, {"author_position": "last", "author": {"id": "https://openalex.org/A2156506640", "display_name": "Niels Petersen", "orcid": "https://orcid.org/0000-0001-6272-6059"}, "institutions": [{"id": "https://openalex.org/I177969490", "display_name": "University of Southern Denmark", "ror": "https://ror.org/03yrrjy16", "country_code": "DK", "type": "education"}], "is_corresponding": None, "raw_affiliation_string": "Department of Business and Economics, and COHERE, University of Southern Denmark, Campusvej 55, Odense M, DK-5230, Denmark"}], "publication_year": 2018}
-
-
-# # Concatenate the dictionaries
-# concatenated_data = {}
-# concatenated_data.update(data1)
-# concatenated_data.update(data2)                        
-                        
-# with gzip.open("sample_strange.json.gz", "wt") as gz_file:
-#     json.dump(concatenated_data, gz_file)                        
-
-
-    
-# with gzip.open('sample_strange.json.gz', 'rt') as f,\
-#         gzip.open('sample_strange_output.csv.gz', 'wt') as sample:
-
-#             writer = csv.writer(sample)
-#             writer.writerow(['oa_id', 'oa_count', 'affiliation_string', 'ror_id'])  # Header
-
-#             for line in f:
-#                 z = 0
-#                 data = json.loads(line) 
-#                 oa_id = data.get('id')
-
-#                 for authorship in data['authorships']:
-
-#                     ror_list = []  # List to store multiple ror values
-
-#                     for institution in authorship['institutions']:
-#                         ror = institution.get('ror')
-#                         if ror:
-#                             ror_list.append(ror)
-#                             z+=1
-#                         else:
-#                             ror_list.append('NA')
-                            
-
-#                     raw_affiliations = authorship.get('raw_affiliation_strings') or authorship.get(
-#                                 'raw_affiliation_string')
-    
-
-#                     for ror in ror_list:
-#                         print(ror)
-#                         writer.writerow([oa_id,z, raw_affiliations, ror])
-                        
-
-
-    
     
--- stage_3.py
+++ stage_3.py
@@ -6,9 +6,8 @@ Created on Mon Jul  3 19:27:16 2023
 @author: aurelasinanaj
 """
 
-import os
 import gzip
-import json
+from datetime import datetime
 import csv
 
 from s2aff.ror import RORIndex
@@ -16,37 +15,77 @@ from s2aff.model import NERPredictor, PairwiseRORLightGBMReranker
 
     
 
-# Open the input CSV file and create an output CSV file
+# open the input CSV file and create an output csv file
 input_file = 'sample.csv.gz'
 output_file = 'predicted_values.csv.gz'
 
+# with gzip.open(input_file, 'rt') as csv_in, gzip.open(output_file, 'wt', newline='') as csv_out:
+#     reader = csv.DictReader(csv_in)
+#     writer = csv.DictWriter(csv_out, fieldnames=reader.fieldnames + ['s2aff_perdiction'])
+
+#     writer.writeheader()  # wite the header for output file
+    
+#     line_count = 0
+#     print_count = 100  #count for printing date and time, each 100 lines
+
+#     for row in reader:
+#         # checking non-empty raw affiliation string
+#         if row['affiliation_string']:
+#             affiliation_string = row['affiliation_string'] 
+#             ner_predictor = NERPredictor(use_cuda=False)
+#             ror_index = RORIndex()  
+#             pairwise_model = PairwiseRORLightGBMReranker(ror_index)
+#             candidates, scores = ror_index.get_candidates_from_raw_affiliation(affiliation_string, ner_predictor)
+#             reranked_candidates, reranked_scores = pairwise_model.predict(affiliation_string, candidates[:100], scores[:100])
+
+#             # process the computed values and update the corresponding row
+#             s2aff_perdiction = []
+#             for i, j in zip(reranked_candidates[:5], reranked_scores[:5]):
+#                 s2aff_perdiction.append(f"{ror_index.ror_dict[i]['id']}: {j}")
+
+#             row['s2aff_perdiction'] = ', '.join(s2aff_perdiction) 
+#             writer.writerow(row)
+            
+#         line_count += 1
+        
+#         if line_count % print_count == 0:
+#             print(f"Lines written:{line_count} - Date and Time:{datetime.now()}")
+            
+# print("End.")
+            
+                   
 with gzip.open(input_file, 'rt') as csv_in, gzip.open(output_file, 'wt', newline='') as csv_out:
     reader = csv.DictReader(csv_in)
-    writer = csv.DictWriter(csv_out, fieldnames=reader.fieldnames + ['s2aff_perdiction'])
+    writer = csv.DictWriter(csv_out, fieldnames=reader.fieldnames + ['s2aff_prediction'])
 
-    writer.writeheader()  # Write the header to the output file
+    writer.writeheader()  # write the header for the output file
+
+    line_count = 0
+    print_count = 100  # count for printing date and time, every 100 lines
 
     for row in reader:
         # checking non-empty raw affiliation string
         if row['affiliation_string']:
-            affiliation_string = row['affiliation_string'] 
+            affiliation_string = row['affiliation_string']
             ner_predictor = NERPredictor(use_cuda=False)
-            ror_index = RORIndex()  
+            ror_index = RORIndex()
             pairwise_model = PairwiseRORLightGBMReranker(ror_index)
             candidates, scores = ror_index.get_candidates_from_raw_affiliation(affiliation_string, ner_predictor)
             reranked_candidates, reranked_scores = pairwise_model.predict(affiliation_string, candidates[:100], scores[:100])
 
-            # Process the computed values and update the corresponding row
-            s2aff_perdiction = []
+            # process the computed values and update the corresponding row
+            s2aff_prediction = {}
             for i, j in zip(reranked_candidates[:5], reranked_scores[:5]):
-                s2aff_perdiction.append(f"{ror_index.ror_dict[i]['id']}: {j}")
+                s2aff_prediction[ror_index.ror_dict[i]['id']] = j
 
-            row['s2aff_perdiction'] = ', '.join(s2aff_perdiction)
+            row['s2aff_prediction'] = s2aff_prediction
             writer.writerow(row)
-                    
-                    
-                    
-                    
-                    
+
+        line_count += 1
+
+        if line_count % print_count == 0:
+            print(f"Lines written: {line_count} - Date and Time: {datetime.now()}")
+
+print("End.")                 
                     
                     

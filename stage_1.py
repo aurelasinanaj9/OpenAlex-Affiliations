88f92c36ca6b36e63f3caa718cc3b852dcfd90ab
aurelasinanaj <aurela.sinanaj@studio.unibo.it>
2023-07-11 11:18:08 +0200

    new file

--- stage_1.py
+++ stage_1.py
@@ -9,91 +9,8 @@ Created on Tue Jul  4 14:11:25 2023
 
 import os
 import gzip
-import json
-import random
 
-
-# # getting a random sample
-# folder_path = 'output'
-# lines_to_extract = 10000 # total number of lines (papers) to extract
-
-# output_file = 'output.json.gz'
-# output_file_path = os.path.join(folder_path, output_file)
-
-# with gzip.open(output_file_path, 'wt') as output:
-#     lines_written = 0
-
-#     for root, dirs, files in os.walk(folder_path):
-#         for file in files:
-#             if file.startswith('matched'):
-#                 file_path = os.path.join(root, file)
-                
-
-#                 with gzip.open(file_path, 'rt') as f:
-#                     file_content = f.read()
-#                     data = json.loads(file_content)
-
-#                     if data:
-#                         random_line = random.choice(data)
-#                         output.write(json.dumps(random_line) + '\n')
-#                         lines_written += 1
-                    
-#                     if lines_written >= lines_to_extract:
-#                         break
-
-
-#         if lines_written >= lines_to_extract:
-#             break
-
-# print("Extraction complete. Lines saved in:", output_file_path)
-
-
-# with gzip.open('output/updated_date=2023-02-01/matched_part_013.json.gz', 'rt') as f,\
-#         gzip.open('output/output.json.gz', 'wt') as sample:
-#             for index, line in enumerate(f): 
-#                 sample.write(line)
-        
-#                 if index + 1 >= 10000:
-#                     break
-            
-# print("Extraction complete. Lines saved")
-
-output_file = 'output/output.json.gz'
-i = 0
-length = 1
-position = 0
-
-with gzip.open(output_file, 'wt') as sample:
-    
-    length += 1
-    position += 1
-    for folder_name in os.listdir('output'):
-        folder_path = os.path.join('output', folder_name)
-        
-        if os.path.isdir(folder_path) and folder_name.startswith('updated_date='):
-            
-            for file_name in os.listdir(folder_path):
-                if file_name.startswith('matched'):
-                    file_path = os.path.join(folder_path, file_name)
-                    
-                    with gzip.open(file_path, 'rt') as f:
-                        lines = f.readlines()
-                        if len(lines) >= length:
-                            sample.write(lines[position])
-                            i += 1
-                            
-                
-                        if i >= 10000:
-                            break
-                            
-            if i >= 10000:
-                break
-            
-                        
-                            
-
-print("Extraction complete. Lines saved.")
-
+# retrieving the first line from all files, then second and so on...
 
 output_file = 'output/output.json.gz'
 lines_to_retrieve = 10000
@@ -113,7 +30,7 @@ with gzip.open(output_file, 'wt') as sample:
 
                         with gzip.open(file_path, 'rt') as f:
                             lines = f.readlines()
-                            if len(lines) > line_position:
+                            if len(lines) > line_position: # checking there are enough lines
                                 sample.write(lines[line_position])
                                 line_count += 1
 
@@ -125,7 +42,7 @@ with gzip.open(output_file, 'wt') as sample:
 
         line_position += 1
 
-print("Extraction complete. Lines saved.")
+print("Extraction complete, lines saved.")
         
         
 
